e42d49da3e2d399b33f03cc0793ab3c95b41ff16
es-smail <ismailsarrar7@gmail.com>
2023-01-21 19:55:37 +0100

    add button of colors

--- app/src/main/java/com/example/controle/MainActivity.java
+++ app/src/main/java/com/example/controle/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        
+
         btn_reinitializer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
